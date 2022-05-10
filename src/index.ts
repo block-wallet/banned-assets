ecd5b634af2ac98183e5f66bd9612b7db0f215f9
Rodrigo <10286502+rodr-r@users.noreply.github.com>
2022-05-10 11:12:48 -0300

    fixing fs

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 import { isValidAddress, toChecksumAddress } from 'ethereumjs-util';
-import excludedTokens from './assets-list';
+import { readFileSync } from 'fs';
+import { join } from 'path';
+
+const _readFileSync = (path: string) => JSON.parse(readFileSync(path, 'utf8'));
+
+const ASSETS_LIST_PATH = join(__dirname, '..', 'assets-list.json');
+
+const excludedTokens: { [chainId in string]: string[] } = _readFileSync(ASSETS_LIST_PATH);
 
 const isTokenExcluded = (chainId: number, address: string): boolean => {
   if (!isValidAddress(address)) {
